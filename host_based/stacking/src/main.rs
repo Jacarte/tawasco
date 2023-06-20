48522e36dd93fc9df6ee88ea36c15611002a27e0
Javier Cabrera <xppcoder@gmail.com>
2023-06-20 22:38:25 +0200

    FMT

--- host_based/stacking/src/main.rs
+++ host_based/stacking/src/main.rs
@@ -2,9 +2,14 @@
 use anyhow::Context;
 use clap::Parser;
 use core::sync::atomic::Ordering::{Relaxed, SeqCst};
-use rand::Rng;
 use rand::rngs::SmallRng;
+use rand::Rng;
+use rand::SeedableRng;
+use std::borrow::Borrow;
+use std::borrow::BorrowMut;
 use std::collections::hash_map::DefaultHasher;
+use std::collections::BTreeMap;
+use std::collections::HashSet;
 use std::ffi::OsStr;
 use std::fmt::Display;
 use std::hash::{Hash, Hasher};
@@ -15,11 +20,6 @@ use std::time::Duration;
 use std::{collections::HashMap, sync::Arc};
 use std::{panic, process};
 use wasm_mutate::WasmMutate;
-use std::collections::HashSet;
-use std::borrow::BorrowMut;
-use std::borrow::Borrow;
-use std::collections::BTreeMap;
-use rand::SeedableRng;
 
 /// # Stacking for wasm-mutate.
 ///
@@ -54,13 +54,16 @@ struct Options {
     #[arg(long = "check-args")]
     check_args: Vec<String>,
 
+    /// Execution fuel. Somehow like a timeout
+    #[arg(long = "fuel", default_value = "0")]
+    fuel: u64,
+
     /// The output Wasm binary.
     output: PathBuf,
 }
 
-fn swap(current: &mut Vec<u8>,
-    new_interesting: Vec<u8>) {
-        *current = new_interesting;
+fn swap(current: &mut Vec<u8>, new_interesting: Vec<u8>) {
+    *current = new_interesting;
 }
 
 struct Stacking {
@@ -69,6 +72,7 @@ struct Stacking {
     check_args: Vec<String>,
     original_state: Option<eval::ExecutionResult>,
     index: usize,
+    fuel: u64,
     count: usize,
     rnd: SmallRng,
     // The hashes will prevent regression and non performed transformations
@@ -76,8 +80,16 @@ struct Stacking {
 }
 
 impl Stacking {
-    
-    pub fn new(current: Vec<u8>, count: usize, seed: u64, remove_cache: bool, cache_dir: String, check_args: Vec<String>, check_io: bool) -> Self {
+    pub fn new(
+        current: Vec<u8>,
+        count: usize,
+        seed: u64,
+        remove_cache: bool,
+        cache_dir: String,
+        check_args: Vec<String>,
+        check_io: bool,
+        fuel: u64,
+    ) -> Self {
         // Remove db if exist
         if remove_cache {
             std::fs::remove_dir_all(&cache_dir.clone());
@@ -88,8 +100,11 @@ impl Stacking {
             .cache_capacity(/* 4Gb */ 4 * 1024 * 1024 * 1024);
 
         let original_state = if check_io {
-            match eval::execute_single(&current, check_args.clone()) {
-                Some(it) => Some(it),
+            match eval::execute_single(&current, check_args.clone(), fuel) {
+                Some(it) => {
+                    eprintln!("Original time {}ns", it.5.as_nanos());
+                    Some(it)
+                }
                 None => {
                     eprintln!("Could not execute the original");
                     process::exit(1);
@@ -105,6 +120,7 @@ impl Stacking {
             check_args,
             original_state,
             index: 0,
+            fuel,
             count,
             rnd: SmallRng::seed_from_u64(seed),
             // Set the cache size to 3GB
@@ -113,11 +129,10 @@ impl Stacking {
     }
 
     pub fn next(&mut self) {
-        
         let mut new = self.current.clone();
-        
+
         // Mutate
-        let mut wasmmutate = WasmMutate::default();    
+        let mut wasmmutate = WasmMutate::default();
         let mut wasmmutate = wasmmutate.preserve_semantics(true);
 
         let seed = self.rnd.gen();
@@ -127,16 +142,11 @@ impl Stacking {
         let wasm = wasmmutate.run(&cp);
 
         match wasm {
-
             Ok(it) => {
-
                 // Get the first one only
                 for w in it {
-
                     match w {
-
                         Ok(b) => {
-
                             // Check if the hash was not generated before
                             let hash = blake3::hash(&b);
                             let hash = hash.as_bytes().to_vec();
@@ -149,12 +159,17 @@ impl Stacking {
                             }
 
                             if let Some(original_state) = &self.original_state {
-                                if !eval::assert_same_evaluation(&self.original, &b, self.check_args.clone()) {
+                                if !eval::assert_same_evaluation(
+                                    &self.original,
+                                    &b,
+                                    self.check_args.clone(),
+                                    self.fuel,
+                                ) {
                                     break;
                                 }
                             }
                             self.hashes.insert(hash, b"1");
-                            
+
                             // Execute to see semantic equivalence
 
                             self.current = b.clone();
@@ -164,19 +179,15 @@ impl Stacking {
                                 eprintln!("{} mutations", self.index);
                             }
 
-                            eprintln!("=== TRANSFORMED {}",  self.index);
+                            eprintln!("=== TRANSFORMED {}", self.index);
                             break;
-
                         }
                         Err(e) => {
                             // We could not mutate the wasm, we skip it
                         }
                     }
-
                 }
-                
-
-            },
+            }
             Err(e) => {
                 // We could not mutate the wasm, we skip it
             }
@@ -192,7 +203,16 @@ fn main() -> anyhow::Result<()> {
     // load the bytes from the input file
     let bytes = std::fs::read(&opts.input).context("Could not read the input file")?;
 
-    let mut stack = Stacking::new(bytes, opts.count, opts.seed, opts.remove_cache, opts.cache_folder, opts.check_args, opts.check_io);
+    let mut stack = Stacking::new(
+        bytes,
+        opts.count,
+        opts.seed,
+        opts.remove_cache,
+        opts.cache_folder,
+        opts.check_args,
+        opts.check_io,
+        opts.fuel,
+    );
 
     loop {
         stack.next();
@@ -200,36 +220,36 @@ fn main() -> anyhow::Result<()> {
         if stack.index % opts.step == 0 {
             let name = format!("{}.{}.wasm", opts.output.to_str().unwrap(), stack.index);
             // Write the current to fs
-            std::fs::write(&name, stack.current.clone()).context("Could not write the output file")?;
+            std::fs::write(&name, stack.current.clone())
+                .context("Could not write the output file")?;
 
             eprintln!("=== STACKED");
         }
         if stack.index == opts.count {
-            break
+            break;
         }
     }
 
     // Assert that we have X different mutations
     // assert!(stack.hashes.len() == opts.count);
-    
+
     // Write the current to fs
     std::fs::write(&opts.output, stack.current).context("Could not write the output file")?;
     Ok(())
 }
 
-
 // Somesort of the same as wasm-mutate fuzz
 mod eval {
-    use stdio_override::StdoutOverride;
-    use stdio_override::StderrOverride;
     use std::fs;
     use std::hash::Hash;
     use std::sync::Arc;
+    use stdio_override::StderrOverride;
+    use stdio_override::StdoutOverride;
     // ./target/release/stacking tests/wb_challenge.wasm stacking.sym --seed 100 -c 2 -v 1000 --check-args wb_challenge.wasm --check-args 00 --check-args 01 --check-args 02 --check-args 03 --check-args 04 --check-args 05 --check-args 06 --check-args 07 --check-args 08 --check-args 09 --check-args 0a --check-args 0b --check-args 0c --check-args 0d --check-args 0e --check-args 0f
-    use wasmtime_wasi::sync::WasiCtxBuilder;
-    use wasmtime_wasi::WasiCtx;
     use std::collections::hash_map::DefaultHasher;
     use std::hash::Hasher;
+    use wasmtime_wasi::sync::WasiCtxBuilder;
+    use wasmtime_wasi::WasiCtx;
 
     fn get_current_working_dir() -> std::io::Result<std::path::PathBuf> {
         std::env::current_dir()
@@ -245,11 +265,27 @@ mod eval {
         linker.clone()
     }
 
-    pub type ExecutionResult = (wasmtime::Store<WasiCtx>, String, String, wasmtime::Module, wasmtime::Instance);
+    pub type ExecutionResult = (
+        wasmtime::Store<WasiCtx>,
+        String,
+        String,
+        wasmtime::Module,
+        wasmtime::Instance,
+        std::time::Duration,
+    );
 
-    pub fn execute_single(wasm: &[u8], args: Vec<String>) -> Option<ExecutionResult> {
+    pub fn execute_single(wasm: &[u8], args: Vec<String>, fuel: u64) -> Option<ExecutionResult> {
         let mut config = wasmtime::Config::default();
+        let config = config.strategy(wasmtime::Strategy::Cranelift);
+
+        // Set no opt for faster execution
+        let config = config.cranelift_opt_level(wasmtime::OptLevel::None);
+
         config.cranelift_nan_canonicalization(true);
+        if fuel > 0 {
+            config.consume_fuel(true);
+        }
+
         // config.consume_fuel(true);
 
         let engine = wasmtime::Engine::new(&config).unwrap();
@@ -263,19 +299,24 @@ mod eval {
 
         let mut wasi = WasiCtxBuilder::new()
             .inherit_stdio()
-            .args( &args)
+            .args(&args)
             .unwrap()
-            // Preopen in the CWD 
+            // Preopen in the CWD
             .preopened_dir(
-                wasmtime_wasi::sync::Dir::open_ambient_dir(folder_of_bin.clone(), wasmtime_wasi::sync::ambient_authority()).unwrap()
-                , ".")
+                wasmtime_wasi::sync::Dir::open_ambient_dir(
+                    folder_of_bin.clone(),
+                    wasmtime_wasi::sync::ambient_authority(),
+                )
+                .unwrap(),
+                ".",
+            )
             .unwrap()
             .build();
 
         let mut linker = create_linker(&engine);
         let stdout_file = "./stdout.txt";
         let stderr_file = "./stderr.txt";
-            
+
         // Recreate the file
         let _ = std::fs::File::create(&stdout_file);
         let _ = std::fs::File::create(&stderr_file);
@@ -284,57 +325,68 @@ mod eval {
         let guarderr = StderrOverride::override_file(stderr_file).unwrap();
 
         let mut store1 = wasmtime::Store::new(&engine, wasi.clone());
-  
+        if fuel > 0 {
+            store1.add_fuel(fuel).unwrap();
+            store1.out_of_fuel_trap();
+        }
+
         let instance1 = linker.instantiate(&mut store1, &module).unwrap();
 
-        let func1 = instance1
-            .get_func(&mut store1, "_start")
-            .unwrap();
+        let func1 = instance1.get_func(&mut store1, "_start").unwrap();
 
-        let r1 = func1.call(&mut store1, &mut [], &mut [])
-            .unwrap();   
+        let now = std::time::Instant::now();
+        let r1 = func1.call(&mut store1, &mut [], &mut []).unwrap();
+        let elapsed = now.elapsed();
 
         let stdout = fs::read_to_string(stdout_file).expect("Cannot read stdout");
         let stderr = fs::read_to_string(stderr_file).expect("Cannot read stderr");
         drop(guardout);
         drop(guarderr);
-        Some((store1, stdout.into(), stderr.into(), module, instance1))
+        Some((
+            store1,
+            stdout.into(),
+            stderr.into(),
+            module,
+            instance1,
+            elapsed,
+        ))
     }
 
     /// Compile, instantiate, and evaluate both the original and mutated Wasm.
     ///
     /// We should get identical results because we told `wasm-mutate` to preserve
     /// semantics.
-    pub fn assert_same_evaluation(original_wasm: &[u8], mutated_wasm: &[u8], args: Vec<String>) -> bool {
-        
-        match (execute_single(original_wasm, args.clone()), execute_single(mutated_wasm, args.clone())) {
-            (Some((mut store1, stdout1, stderr1, mod1, instance1)), Some((mut store2, stdout2, stderr2, _mod2, instance2))) => {
-                
+    pub fn assert_same_evaluation(
+        original_wasm: &[u8],
+        mutated_wasm: &[u8],
+        args: Vec<String>,fuel: u64
+    ) -> bool {
+        match (
+            execute_single(original_wasm, args.clone(), fuel),
+            execute_single(mutated_wasm, args.clone(), fuel),
+        ) {
+            (
+                Some((mut store1, stdout1, stderr1, mod1, instance1, _)),
+                Some((mut store2, stdout2, stderr2, _mod2, instance2, time2)),
+            ) => {
                 if stdout1 != stdout2 || stderr1 != stderr2 {
                     eprintln!("Std is not the same");
-                    return false
+                    return false;
                 }
                 // Now we compare the stores
-                if !assert_same_state(
-                    &mod1,
-                    &mut store1,
-                    instance1,
-                    &mut store2,
-                    instance2,
-                ) {
+                if !assert_same_state(&mod1, &mut store1, instance1, &mut store2, instance2) {
                     eprintln!("Invalid state");
-                    return false
+                    return false;
                 };
                 // Compare the memories
 
                 // Compare the globals
 
+                eprintln!("Time {}ns", time2.as_nanos());
 
-                return true
+                return true;
             }
-            _ => {
-                return false
-            }
+            _ => return false,
         }
     }
 
@@ -363,7 +415,7 @@ mod eval {
 
                     if !assert_val_eq(&orig, &mutated) {
                         eprintln!("Globals are not the same");
-                        return false
+                        return false;
                     }
                 }
                 wasmtime::ExternType::Memory(_) => {
@@ -386,17 +438,16 @@ mod eval {
 
                     if orig != mutated {
                         eprintln!("original and mutated Wasm memories diverged");
-                        return false
+                        return false;
                     }
                 }
                 _ => continue,
             }
         }
 
-        return true
+        return true;
     }
 
-    
     /*
     fn assert_same_calls(
         orig_module: &wasmtime::Module,
@@ -461,8 +512,7 @@ mod eval {
 
      */
 
-
-     fn assert_val_eq(orig_val: &wasmtime::Val, mutated_val: &wasmtime::Val) -> bool {
+    fn assert_val_eq(orig_val: &wasmtime::Val, mutated_val: &wasmtime::Val) -> bool {
         match (orig_val, mutated_val) {
             (wasmtime::Val::I32(o), wasmtime::Val::I32(m)) => return o == m,
             (wasmtime::Val::I64(o), wasmtime::Val::I64(m)) => return o == m,
@@ -476,21 +526,20 @@ mod eval {
                 let m = f64::from_bits(*m);
                 return o == m || (o.is_nan() && m.is_nan());
             }
-            (wasmtime::Val::V128(o), wasmtime::Val::V128(m)) => {
-                return o == m
-            }
+            (wasmtime::Val::V128(o), wasmtime::Val::V128(m)) => return o == m,
             (wasmtime::Val::ExternRef(o), wasmtime::Val::ExternRef(m)) => {
                 return o.is_none() == m.is_none()
             }
             (wasmtime::Val::FuncRef(o), wasmtime::Val::FuncRef(m)) => {
                 return o.is_none() == m.is_none()
             }
-            (o, m) => { eprintln!(
-                "mutated and original Wasm diverged: orig = {:?}; mutated = {:?}",
-                o, m,
-            );
-            return false},
+            (o, m) => {
+                eprintln!(
+                    "mutated and original Wasm diverged: orig = {:?}; mutated = {:?}",
+                    o, m,
+                );
+                return false;
+            }
         }
-
     }
 }
