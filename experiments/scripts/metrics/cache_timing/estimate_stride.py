faf7d1ca290b2643519871daf4ce8cc115ddf039
Javier Cabrera <xppcoder@gmail.com>
2023-06-22 08:41:16 +0200

    Adding cache time BER script

---
+++ experiments/scripts/metrics/cache_timing/estimate_stride.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import re
+import subprocess
+
+def BER(k1, k2):
+    MISS = 0
+    for i, c in enumerate(k2):
+        if c != k1[i]:
+            MISS += 1
+    return MISS / len(k1), MISS
+
+
+def estimate_stride_size(f, t):
+    # read the file
+
+    AVG = 0
+    NUM = 100
+    print(t, NUM)
+    for j in range(NUM):
+        ch = subprocess.run(['host', f], stdout=subprocess.PIPE, env = {
+            'TRIES': f'{t}',
+        })
+        r, c = BER("My password", ch.stdout.decode())
+        AVG += 1 - r
+        print(t,  ch.stdout.decode(), 1-r, 11-c)
+    print(AVG/NUM)
+        
+        
+
+
+
+
+
+if __name__ == "__main__":
+    f = sys.argv[1]
+    size = int(sys.argv[2])
+    estimate_stride_size(f, size)
