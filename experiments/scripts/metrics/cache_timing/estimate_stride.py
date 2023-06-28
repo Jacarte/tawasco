611ce5ffefaebce58bbd9e265e3fde33a0aaa04e
Javier Cabrera <xppcoder@gmail.com>
2023-06-28 14:51:29 +0200

    WIP

--- experiments/scripts/metrics/cache_timing/estimate_stride.py
+++ experiments/scripts/metrics/cache_timing/estimate_stride.py
@@ -13,7 +13,7 @@ def BER(k1, k2):
     return MISS / len(k1), MISS
 
 
-def estimate_stride_size(f, t, SECRET="My password"):
+def estimate_stride_size(f, t, t2, SECRET="My password"):
     # read the file
 
     AVG = 0
@@ -22,6 +22,7 @@ def estimate_stride_size(f, t, SECRET="My password"):
     for j in range(NUM):
         ch = subprocess.run([HOST, f], stdout=subprocess.PIPE, env = {
             'TRIES': f'{t}',
+            'TRIALS': f'{t2}',
             **os.environ
         })
         r, c = BER(SECRET, ch.stdout.decode())
@@ -40,7 +41,8 @@ def estimate_stride_size(f, t, SECRET="My password"):
 if __name__ == "__main__":
     f = sys.argv[1]
     size = int(sys.argv[2])
-    SECRET = sys.argv[3]
-    r = estimate_stride_size(f, size, SECRET)
+    t2 = int(sys.argv[3])
+    SECRET = sys.argv[4]
+    r = estimate_stride_size(f, size, t2, SECRET)
     sys.stderr.write("%s\n"%(r, ))
     print(r)
